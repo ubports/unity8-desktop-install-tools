6523fa852dbcb46065d9b9095bc618a41881e718
Marius Gripsgard <mariogrip@ubuntu.com>
2017-12-30 11:43:39 +0100

    Add enable mir 0.29 script

---
+++ enable-mir0-29.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## We should die on any errors!
+set -e
+
+## Check if we are on xenial
+source /etc/os-release
+if [ $UBUNTU_CODENAME != "xenial" ]; then
+    echo "This is only supported on xenial"
+    exit 1
+fi
+
+## if no root, Sudo me up!
+[ $(id -u) -ne 0 ] && exec sudo $0 $SUDOARGS
+
+echo "deb http://repo.ubports.com/ xenial_-_mir26 main" >> /etc/apt/sources.list.d/ubports.list
+apt update
+
+echo "I will do a dist upgrade!"
+apt dist-upgrade
+
+apt autoremove -y
+echo "--- DONE ---"
