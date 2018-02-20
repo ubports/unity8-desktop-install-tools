a378ac0f25ccb14cfd59bd050fa9b5900d4938c3
Marius Gripsgard <mariogrip@ubuntu.com>
2018-02-20 15:18:48 +0100

    Add support for bionic

--- install.sh
+++ install.sh
@@ -3,48 +3,77 @@
 ## We should die on any errors!
 set -e
 
-## Check if we are on xenial
+## Check if we are on xenial or bionic
 source /etc/os-release
-if [ $UBUNTU_CODENAME != "xenial" ]; then
-    echo "This is only supported on xenial"
+if [ $UBUNTU_CODENAME != "xenial" ] && [ $UBUNTU_CODENAME != "bionic" ]; then
+    echo "This is only supported on Ubuntu xenial and bionic"
     exit 1
 fi
 
 ## if no root, Sudo me up!
 [ $(id -u) -ne 0 ] && exec sudo $0 $SUDOARGS
 
-## Setup ubports repo
-echo "deb http://repo.ubports.com/ xenial main" > /etc/apt/sources.list.d/ubports.list
+function do_install {
+  ## Do apt update
+  apt update
 
-## Add temp repo until merged into the main repo
-echo "deb http://repo.ubports.com/ xenial_-_mir26 main" >> /etc/apt/sources.list.d/ubports.list
+  ## Do apt upgrade
+  apt upgrade -y --allow-downgrades
 
-## Add pin 
-cat >/etc/apt/preferences.d/ubports.pref <<EOL 
-Package: *
-Pin: origin repo.ubports.com
-Pin-Priority: 2000
+  echo "If you get an option to select display manager, please select lightdm!"
+  read -p "Press enter to continue"
 
-Package: *
-Pin: release a=xenial_-_mir26
-Pin-Priority: 5000
+  ## Install unity8 desktop session and lightdm
+  apt install -y unity8-desktop-session lightdm
+
+  ## Done, let's tell the user
+  echo "------ DONE ------"
+  echo "You can now logout and select unity8"
+}
+
+function setup_repo {
+  ## Setup ubports repo
+  echo "deb http://repo.ubports.com/ $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/ubports
+
+  ## Add ubports keyring
+  wget http://repo.ubports.com/keyring.gpg -O - | apt-key add -
+}
+
+function install {
+  setup_repo
+  do_install
+}
+
+
+function xenial_install {
+  setup_repo
+
+  ## Add temp repo until merged into the main repo
+  echo "deb http://repo.ubports.com/ xenial_-_mir26 main" >> /etc/apt/sources.list.d/ubports.list
+
+  ## This will be removed once all packages have been moved to ubports repo
+  add-apt-repository ppa:ci-train-ppa-service/stable-phone-overlay -y
+
+  ## Add pin
+  cat >/etc/apt/preferences.d/ubports.pref << EOL
+  Package: *
+  Pin: origin repo.ubports.com
+  Pin-Priority: 2000
+
+  Package: *
+  Pin: release a=xenial_-_mir26
+  Pin-Priority: 5000
 EOL
 
-## Add ubports keyring
-wget http://repo.ubports.com/keyring.gpg -O - | apt-key add -
+  do_install
+}
 
-## This will be removed once all packages have been moved to ubports repo
-add-apt-repository ppa:ci-train-ppa-service/stable-phone-overlay -y
 
-## Do apt update
-apt update
-
-## Do apt upgrade
-apt upgrade -y
-
-## Install unity8 desktop sessions
-apt install -y unity8-desktop-session-mir unity8-desktop-session
-
-## Done, let's tell the user
-echo "------ DONE ------"
-echo "You can now logout and select unity8"
+case $UBUNTU_CODENAME in
+  "xenial")
+    xenial_install
+  ;;
+  "bionic")
+    install
+   ;;
+esac
