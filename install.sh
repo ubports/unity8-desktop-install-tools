187278f0035ba714e91f9f241b1e1f6c71aa28f9
Marius Gripsgard <mariogrip@ubuntu.com>
2018-02-21 18:07:30 +0100

    Use sudo only on commands that need it

--- install.sh
+++ install.sh
@@ -10,21 +10,18 @@ if [ $UBUNTU_CODENAME != "xenial" ] && [ $UBUNTU_CODENAME != "bionic" ]; then
     exit 1
 fi
 
-## if no root, Sudo me up!
-[ $(id -u) -ne 0 ] && exec sudo $0 $SUDOARGS
-
 function do_install {
   ## Do apt update
-  apt update
+  sudo apt update
 
   ## Do apt upgrade
-  apt upgrade -y --allow-downgrades
+  sudo apt upgrade -y --allow-downgrades
 
   echo "If you get an option to select display manager, please select lightdm!"
   read -p "Press enter to continue"
 
   ## Install unity8 desktop session and lightdm
-  apt install -y unity8-desktop-session lightdm
+  sudo apt install -y unity8-desktop-session lightdm
 
   ## Done, let's tell the user
   echo "------ DONE ------"
@@ -33,10 +30,10 @@ function do_install {
 
 function setup_repo {
   ## Setup ubports repo
-  echo "deb http://repo.ubports.com/ $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/ubports
+  echo "deb http://repo.ubports.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ubports.list
 
   ## Add ubports keyring
-  wget http://repo.ubports.com/keyring.gpg -O - | apt-key add -
+  wget http://repo.ubports.com/keyring.gpg -O - | sudo apt-key add -
 }
 
 function install {
@@ -49,13 +46,13 @@ function xenial_install {
   setup_repo
 
   ## Add temp repo until merged into the main repo
-  echo "deb http://repo.ubports.com/ xenial_-_mir26 main" >> /etc/apt/sources.list.d/ubports.list
+  echo "deb http://repo.ubports.com/ xenial_-_mir26 main" | sudo tee -a /etc/apt/sources.list.d/ubports.list
 
   ## This will be removed once all packages have been moved to ubports repo
-  add-apt-repository ppa:ci-train-ppa-service/stable-phone-overlay -y
+  sudo add-apt-repository ppa:ci-train-ppa-service/stable-phone-overlay -y
 
   ## Add pin
-  cat >/etc/apt/preferences.d/ubports.pref << EOL
+  sudo tee /etc/apt/preferences.d/ubports.pref << EOL
   Package: *
   Pin: origin repo.ubports.com
   Pin-Priority: 2000
