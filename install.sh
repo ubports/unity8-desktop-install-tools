be94c85076537971e21f6349064e457962774075
Simon Quigley <tsimonq2@ubuntu.com>
2018-07-24 06:28:01 -0500

    Clean up the install script.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 ## Check if we are on xenial or bionic
 source /etc/os-release
 if [ $UBUNTU_CODENAME != "xenial" ] && [ $UBUNTU_CODENAME != "bionic" ]; then
-    echo "This is only supported on Ubuntu xenial and bionic"
+    echo "This is only supported on Ubuntu 16.04 and 18.04."
     exit 1
 fi
 
@@ -27,7 +27,7 @@ function do_install {
 
 function setup_repo {
   ## Make sure gnupg is installed
-  sudo apt install gnupg
+  sudo apt install -y gnupg
 
   ## Setup ubports repo
   echo "deb http://repo.ubports.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ubports.list
@@ -41,31 +41,9 @@ function install {
   do_install
 }
 
-
-function xenial_install {
-  setup_repo
-
-  ## Add temp repo until merged into the main repo
-  echo "deb http://repo.ubports.com/ xenial_-_mir26 main" | sudo tee -a /etc/apt/sources.list.d/ubports.list
-
-  ## Add pin
-  sudo tee /etc/apt/preferences.d/ubports.pref << EOL
-  Package: *
-  Pin: origin repo.ubports.com
-  Pin-Priority: 2000
-
-  Package: *
-  Pin: release a=xenial_-_mir26
-  Pin-Priority: 5000
-EOL
-
-  do_install
-}
-
-
 case $UBUNTU_CODENAME in
   "xenial")
-    xenial_install
+    install
   ;;
   "bionic")
     install
